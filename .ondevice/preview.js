83b2352cc85dba802515ec3603df9e43435f16c0
Daniel <daniel.williams@coverflex.com>
2022-02-19 00:07:28 +0000

    fix: adjust versions

--- .ondevice/preview.js
+++ .ondevice/preview.js
@@ -1,5 +1,7 @@
 import { withBackgrounds } from "@storybook/addon-ondevice-backgrounds";
+
 export const decorators = [withBackgrounds];
+
 export const parameters = {
   backgrounds: [
     { name: "plain", value: "white", default: true },
--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react-native';
-import {MyButton} from './Button';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react-native";
+import { MyButton } from "./Button";
+
 const MyButtonMeta: ComponentMeta<typeof MyButton> = {
-  title: 'MyButton',
+  title: "MyButton",
   component: MyButton,
   argTypes: {
-    onPress: {action: 'pressed the button'},
+    onPress: { action: "pressed the button" },
   },
   args: {
-    text: 'Hello world',
+    text: "Hello world",
   },
 };
-export default MyButtonMeta;
-type MyButtonStory = ComponentStory<typeof MyButton>;
-export const Basic: MyButtonStory = args => <MyButton {...args} />;
 
+export default MyButtonMeta;
+
+type MyButtonStory = ComponentStory<typeof MyButton>;
+
+export const Basic: MyButtonStory = (args) => <MyButton {...args} />;
