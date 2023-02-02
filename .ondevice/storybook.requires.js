adbfcf18a9df0d103a523a1f4bc61413f194923e
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2023-02-02 12:21:34 +0000

    feat: update to beta 12

--- .ondevice/storybook.requires.js
+++ .ondevice/storybook.requires.js
@@ -8,6 +8,16 @@ import {
   clearDecorators,
 } from "@storybook/react-native";
 
+global.STORIES = [
+  {
+    titlePrefix: "",
+    directory: "./components",
+    files: "**/*.stories.?(ts|tsx|js|jsx)",
+    importPathMatcher:
+      "^\\.[\\\\/](?:components(?:\\/(?!\\.)(?:(?:(?!(?:^|\\/)\\.).)*?)\\/|\\/|$)(?!\\.)(?=.)[^/]*?\\.stories\\.(?:ts|tsx|js|jsx)?)$",
+  },
+];
+
 import "@storybook/addon-ondevice-notes/register";
 import "@storybook/addon-ondevice-controls/register";
 import "@storybook/addon-ondevice-backgrounds/register";
@@ -38,7 +48,9 @@ try {
 } catch {}
 
 const getStories = () => {
-  return [require("../components/Button/Button.stories.tsx")];
+  return {
+    "./components/Button/Button.stories.tsx": require("../components/Button/Button.stories.tsx"),
+  };
 };
 
 configure(getStories, module, false);
