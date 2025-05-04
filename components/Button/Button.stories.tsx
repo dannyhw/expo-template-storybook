8bae9b589c1dcfd7899c27384f691325e52150cf
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-05-04 17:24:05 +0100

    feat: update

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import React from "react";
 import { View } from "react-native";
 import { MyButton } from "./Button";
 
